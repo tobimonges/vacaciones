cdbbc32e08d53662118cc6ebbe6fa708872c1999
tobimonges <tobiasmonges@pm.me>
2024-12-20 11:33:33 -0300

    proyecto final añadido

---
+++ vacaciones/vacaciones/src/main/java/bootcamp/vacaciones/VacacionesApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.vacaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacacionesApplication.class, args);
+	}
+
+}
---
+++ vacaciones/vacaciones/src/test/java/bootcamp/vacaciones/VacacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.vacaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
