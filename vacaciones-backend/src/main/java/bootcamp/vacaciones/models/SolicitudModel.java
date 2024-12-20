2caf00a1d577d4d3be8127fe53ce33a608f7a4f1
tobimonges <tobiasmonges@pm.me>
2024-12-20 14:23:10 -0300

    UsuarioModel modificado

--- vacaciones-backend/src/main/java/bootcamp/vacaciones/models/SolicitudModel.java
+++ vacaciones-backend/src/main/java/bootcamp/vacaciones/models/SolicitudModel.java
@@ -1,4 +1,24 @@
 package bootcamp.vacaciones.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "solicitud")
 public class SolicitudModel {
+    @Id
+    GeneratedValue(strategy = GenerationType.IDENTITY);
+    @Column(unique = true, nullable = false);
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private UsuarioModel usuario;
+
+
 }
