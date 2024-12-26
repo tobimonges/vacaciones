f3ffbe0c19aaba131398212f4d17f6b8c5453605
tobimonges <tobiasmonges@pm.me>
2024-12-26 10:05:57 -0300

    mejorando usuario

--- vacaciones-backend/src/main/java/bootcamp/vacaciones/controllers/UsuarioController.java
+++ vacaciones-backend/src/main/java/bootcamp/vacaciones/controllers/UsuarioController.java
@@ -1,7 +1,7 @@
 package bootcamp.vacaciones.controllers;
 
 import bootcamp.vacaciones.models.UsuarioModel;
-import bootcamp.vacaciones.services.UsuarioService;
+import bootcamp.vacaciones.services.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,20 +10,57 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/vacaciones")
+
+@CrossOrigin(value = "http://localhost:3000") //para recibir peticiones del front
 public class UsuarioController {
     @Autowired
-    private UsuarioService usuarioService;
+    private IUsuarioService usuarioService;
+
     @GetMapping("/usuarios")
-    public ResponseEntity<List<UsuarioModel>> getUsuario(){
-        return ResponseEntity.ok(usuarioService.getUsuario());
+    public List<UsuarioModel> obtenerUsuarios() {
+        return usuarioService.listarUsuarios();
     }
 
-    @PostMapping
-    public ResponseEntity<UsuarioModel> CrearUSuario(@RequestBody UsuarioModel usuario){
-        UsuarioModel nuevoUSuario = usuarioService.crearUsuario(usuario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoUSuario);
+    @PostMapping("/usuarios")
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario) {
+        return usuarioService.guardarUsuario(usuario);
     }
 
+    @GetMapping("/usuarios/{id}")
+    public ResponseEntity<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id) {
+        UsuarioModel usuario = usuarioService.buscarUsuarioPorId(id);
+        if (usuario == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return ResponseEntity.ok(usuario);
+        }
+    }
+
+    @PutMapping("/usuarios/{id}")
+    public ResponseEntity<UsuarioModel> actualizarUsuario(@PathVariable Long id, @RequestBody UsuarioModel usuarioRecibido) {
+        UsuarioModel usuario = usuarioService.buscarUsuarioPorId(id);
+        if (usuario == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            usuario.setNombre(usuarioRecibido.getNombre());
+            usuario.setApellido(usuarioRecibido.getApellido());
+            usuario.setCorreo(usuarioRecibido.getCorreo());
+            usuario.setContrasena(usuarioRecibido.getContrasena());
+            usuarioService.guardarUsuario(usuario);
+            return ResponseEntity.ok(usuario);
+        }
+    }
+
+    @DeleteMapping("/usuarios/{id}")
+    public ResponseEntity<UsuarioModel> eliminarUsuario(@PathVariable Long id) {
+        UsuarioModel usuario = usuarioService.buscarUsuarioPorId(id);
+        if (usuario == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            usuarioService.eliminarUsuario(usuario);
+            return ResponseEntity.ok(usuario);
+        }
+    }
 
 }
---
+++ vacaciones-backend/src/main/java/bootcamp/vacaciones/services/IUsuarioService.java
@@ -0,0 +1,15 @@
+package bootcamp.vacaciones.services;
+
+import bootcamp.vacaciones.models.UsuarioModel;
+
+import java.util.List;
+
+public interface IUsuarioService {
+    List<UsuarioModel> listarUsuarios();
+
+    UsuarioModel buscarUsuarioPorId(Long idUsuario);
+
+    UsuarioModel guardarUsuario(UsuarioModel usuario);
+
+    void eliminarUsuario(UsuarioModel usuario);
+}
