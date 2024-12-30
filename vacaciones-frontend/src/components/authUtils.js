72acb46cb7439f0698ca2c1e72f364f81a80127f
CDPATINOCoder <cesardavidpatinovera@gmail.com>
2024-12-30 17:33:31 -0300

    authUtils, se agrego la funcion para decodificar el jwt y obtener el id

--- vacaciones-frontend/src/components/authUtils.js
+++ vacaciones-frontend/src/components/authUtils.js
@@ -1,10 +1,11 @@
-import * as jwt_decode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 export const getUsuarioId = () => {
   const token = localStorage.getItem("token");
   if (token) {
     try {
-      const decoded = jwt_decode(token); // Decodifica el token
-      return decoded.usuarioId; // Asegúrate de que el token contenga el campo usuarioId
+      const decoded = jwtDecode(token); // Decodifica el token directamente
+      console.log("Token decodificado:", decoded); // Para depuración
+      return decoded.usuarioId; // Asegúrate de que el token contenga esta información
     } catch (error) {
       console.error("Error al decodificar el token:", error);
       return null;
