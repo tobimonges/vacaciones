53b2ed86c64e214d262e88870a493473e857c1a4
CDPATINOCoder <cesardavidpatinovera@gmail.com>
2025-01-02 10:05:12 -0300

    App.jsx modificado, se agrego la ruta SolicitudDetalle para listar las solicitudes por id de usuario

--- vacaciones-frontend/src/App.jsx
+++ vacaciones-frontend/src/App.jsx
@@ -1,20 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
-import ProtectedRoute from "./components/rutas";
 import NuevaSolicitud from "./components/NuevaSolicitud";
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/*ruta Login */}
-        <Route path="/" element={<Login />} />
-        {/* Ruta para Home */}
-        <Route path="/Home" element={<Home />} />
+import SolicitudDetalle from "./components/SolicitudDetalle";
 
-        <Route path="/NuevaSolicitud" element={<NuevaSolicitud />} />
-      </Routes>
-    </Router>
-  );
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/Home" element={<Home />} />
+                <Route path="/NuevaSolicitud" element={<NuevaSolicitud />} />
+                {/* Ruta para ver detalles de una solicitud */}
+                <Route path="/SolicitudDetalle/:id" element={<SolicitudDetalle />} />
+            </Routes>
+        </Router>
+    );
 }
+
 export default App;
