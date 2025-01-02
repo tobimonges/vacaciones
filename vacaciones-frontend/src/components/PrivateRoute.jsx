1cd8c3b0311dc03f222d4053fee1daa5e4a1deaf
CDPATINOCoder <cesardavidpatinovera@gmail.com>
2025-01-02 17:02:42 -0300

    PrivateRoute agregado, componente para proteger AdminDashboard

--- vacaciones-frontend/src/components/PrivateRoute.jsx
+++ vacaciones-frontend/src/components/PrivateRoute.jsx
@@ -1,15 +1,20 @@
+// PrivateRoute.jsx
+import React from "react";
 import { Navigate } from "react-router-dom";
+import { isAdmin } from "./authUtils";
 
-const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
+const PrivateRoute = ({ children, adminOnly }) => {
+  const token = localStorage.getItem("token");
 
-  if (!isAuthenticated) {
-    localStorage.setItem("isAuthenticated", "false"); // Asegurar que la sesión se cierre
-    alert("No tienes acceso a la página, favor inicie sesión primero.");
-    return <Navigate to="/" replace />; // Redirigir al login
+  if (!token) {
+    return <Navigate to="/" />;
   }
 
-  return children; // Renderizar el contenido protegido
+  if (adminOnly && !isAdmin()) {
+    return <Navigate to="/Home" />;
+  }
+
+  return children;
 };
 
-export default ProtectedRoute;
+export default PrivateRoute;
