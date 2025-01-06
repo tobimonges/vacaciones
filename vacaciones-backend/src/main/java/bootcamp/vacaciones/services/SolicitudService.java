774f36c531614d5d6ffb4d2d34a2f73eeeb9278d
CDPATINOCoder <cesardavidpatinovera@gmail.com>
2025-01-06 15:56:42 -0300

    adjf

--- vacaciones-backend/src/main/java/bootcamp/vacaciones/services/SolicitudService.java
+++ vacaciones-backend/src/main/java/bootcamp/vacaciones/services/SolicitudService.java
@@ -117,58 +117,6 @@ public class SolicitudService implements ISolicitudService {
         return solicitudRepository.save(solicitud);
     }
 
-    public String aprobarSolicitud(Long solicitudId, Long usuarioId) {
-        SolicitudModel solicitud = solicitudRepository.findById(solicitudId)
-                .orElseThrow(() -> new RuntimeException("Solicitud no encontrada"));
-
-        UsuarioModel usuario = usuarioRepository.findById(usuarioId)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-
-        // Validar que el líder asignado apruebe primero
-        if (solicitud.getNumeroAprobaciones() == 0) {
-            if (!solicitud.getLider().getId().equals(usuario.getId())) {
-                throw new RuntimeException("Solo el líder asignado puede aprobar esta solicitud.");
-            }
-            solicitud.setNumeroAprobaciones(1); // Aprobado por el líder
-        }
-        // Validar que el TH apruebe después
-        else if (solicitud.getNumeroAprobaciones() == 1) {
-            if (!"TH".equals(usuario.getRol().getNombre())) {
-                throw new RuntimeException("Solo el rol TH puede aprobar en esta etapa.");
-            }
-            solicitud.setNumeroAprobaciones(2); // Aprobado por TH
-            solicitud.setEstado(true); // Solicitud completamente aprobada
-        } else {
-            throw new RuntimeException("La solicitud ya está completamente aprobada.");
-        }
-
-        solicitudRepository.save(solicitud);
-        return "Solicitud aprobada con éxito.";
-    }
-
-
-    public String rechazarSolicitud(Long solicitudId, Long usuarioId, String comentario) {
-        SolicitudModel solicitud = solicitudRepository.findById(solicitudId)
-                .orElseThrow(() -> new RuntimeException("Solicitud no encontrada"));
-
-        UsuarioModel usuario = usuarioRepository.findById(usuarioId)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-
-        // Validar que el usuario tenga el rol "OPERACIONES"
-        if (!"OPERACIONES".equals(usuario.getRol().getNombre())) {
-            throw new RuntimeException("Solo un usuario con rol OPERACIONES puede añadir comentarios y rechazar la solicitud.");
-        }
-
-        // Actualizar estado de la solicitud
-        solicitud.setEstado(false);
-        solicitud.setRechazado(true);
-        solicitud.setComentario(comentario); // Guardar el motivo del rechazo
-
-        solicitudRepository.save(solicitud);
-        return "La solicitud ha sido rechazada con éxito por el rol OPERACIONES.";
-    }
-
-
 }
 
 
