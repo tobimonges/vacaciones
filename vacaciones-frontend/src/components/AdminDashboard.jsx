8ccc5fba649a354b51528fd19d79c69765ba5c58
jgarcete <jcgarcete2@gmail.com>
2025-01-08 10:45:18 -0300

    Se añaden validaciones en los botones de acciones en funcion a los roles en el apartado AdminDashboard

--- vacaciones-frontend/src/components/AdminDashboard.jsx
+++ vacaciones-frontend/src/components/AdminDashboard.jsx
@@ -18,10 +18,10 @@ const AdminDashboard = () => {
 
       try {
         const response = await axios.get(
-            "http://localhost:8080/vacaciones/solicitudes",
-            {
-              headers: { Authorization: `Bearer ${token}` },
-            }
+          "http://localhost:8080/vacaciones/solicitudes",
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
         );
 
         setSolicitudes(response.data);
@@ -40,32 +40,27 @@ const AdminDashboard = () => {
 
     try {
       const response = await axios.put(
-          `http://localhost:8080/vacaciones/${id}/aprobar?usuarioId=${userId}`,
-          null,
-          {
-            headers: { Authorization: `Bearer ${token}` },
-          }
+        `http://localhost:8080/vacaciones/${id}/aprobar?usuarioId=${userId}`,
+        null,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
       );
 
-      console.log(response.data)
       alert("Solicitud aprobada con éxito.");
-      // Actualizamos la solicitud en tiempo real con los datos del backend
       setSolicitudes((prev) =>
-          prev.map((solicitud) =>
-              solicitud.id === id ? response.data : solicitud
-          )
+        prev.map((solicitud) =>
+          solicitud.id === id ? response.data : solicitud
+        )
       );
     } catch (err) {
       console.error("Error al aprobar solicitud:", err.response.data);
-
-      // Extraer el mensaje de error del backend
       const errorMessage =
-          err.response.data.message || "Ocurrió un error al aprobar la solicitud.";
-
+        err.response.data.message ||
+        "Ocurrió un error al aprobar la solicitud.";
       alert(`Error al aprobar la solicitud: ${errorMessage}`);
     }
   };
-
 
   const handleReject = async (id) => {
     const token = localStorage.getItem("token");
@@ -73,22 +68,24 @@ const AdminDashboard = () => {
 
     try {
       const response = await axios.put(
-          `http://localhost:8080/vacaciones/${id}/rechazar-lider-th?usuarioId=${userId}`,
-          null,
-          {
-            headers: { Authorization: `Bearer ${token}` },
-          }
+        `http://localhost:8080/vacaciones/${id}/rechazar-lider-th?usuarioId=${userId}`,
+        null,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
       );
       alert("Solicitud rechazada con éxito.");
-
-      console.log(response.data)
-      // Actualizar el estado local de las solicitudes
       setSolicitudes((prev) =>
-          prev.map((solicitud) =>
-              solicitud.id === id
-                  ? { ...solicitud, estado: false, rechazado: true, numeroAprobaciones: 0 }
-                  : solicitud
-          )
+        prev.map((solicitud) =>
+          solicitud.id === id
+            ? {
+                ...solicitud,
+                estado: false,
+                rechazado: true,
+                numeroAprobaciones: 0,
+              }
+            : solicitud
+        )
       );
     } catch (err) {
       console.error("Error al rechazar solicitud:", err.response.data);
@@ -107,21 +104,21 @@ const AdminDashboard = () => {
 
     try {
       await axios.put(
-          `http://localhost:8080/vacaciones/${selectedSolicitudId}/rechazar-operador?usuarioId=${userId}`,
-          { comentario },
-          {
-            headers: { Authorization: `Bearer ${token}` },
-          }
+        `http://localhost:8080/vacaciones/${selectedSolicitudId}/rechazar-operador?usuarioId=${userId}`,
+        { comentario },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
       );
       alert("Comentario añadido y solicitud rechazada con éxito.");
       setShowModal(false);
       setComentario("");
       setSolicitudes((prev) =>
-          prev.map((solicitud) =>
-              solicitud.id === selectedSolicitudId
-                  ? { ...solicitud, estado: false, rechazado: true, comentario }
-                  : solicitud
-          )
+        prev.map((solicitud) =>
+          solicitud.id === selectedSolicitudId
+            ? { ...solicitud, estado: false, rechazado: true, comentario }
+            : solicitud
+        )
       );
     } catch (err) {
       console.error("Error al añadir comentario:", err);
@@ -140,56 +137,96 @@ const AdminDashboard = () => {
   };
 
   const getEstadoSolicitud = (solicitud) => {
-    if (solicitud.estado) {
-      return "Aprobado";
+    if (solicitud.numeroAprobaciones === 1) {
+      return "Pendiente a TH"; // Si hay una aprobación, el estado es "Pendiente a TH"
+    } else if (solicitud.estado) {
+      return "Aprobado"; // Si la solicitud está aprobada completamente
     } else if (!solicitud.estado && solicitud.rechazado) {
-      return "Rechazado";
+      return "Rechazado"; // Si la solicitud fue rechazada
     } else {
-      return "Pendiente";
+      return "Pendiente"; // Cualquier otro caso
     }
   };
 
   return (
-      <div className="container-admin">
-        <h4>Panel de Administrador</h4>
-        {error ? (
-            <p className="error">{error}</p>
-        ) : solicitudes.length === 0 ? (
-            <p>No hay solicitudes pendientes.</p>
-        ) : (
-            <table className="admin-table">
-              <thead>
-              <tr>
-                <th>ID</th>
-                <th>Usuario</th>
-                <th>Fecha Inicio</th>
-                <th>Fecha Fin</th>
-                <th>Estado</th>
-                {userRole === "TH" && <th>Aprobada por Líder</th>}
-                <th>Acciones</th>
-              </tr>
-              </thead>
-              <tbody>
-              {solicitudes.map((solicitud) => (
-                  <tr key={solicitud.id}>
-                    <td>{solicitud.id}</td>
-                    <td>{solicitud.usuario.nombre}</td>
-                    <td>{solicitud.fechaInicio}</td>
-                    <td>{solicitud.fechaFin}</td>
-                    <td>{getEstadoSolicitud(solicitud)}</td>
-                    {userRole === "TH" && (
-                        <td>
-                          {solicitud.numeroAprobaciones === 1 || solicitud.numeroAprobaciones === 2
-                              ? "Sí"
-                              : "No"}
-                        </td>
-                    )}
-                    <td>
-                      {userRole === "OPERACIONES" ? (
-                          <button onClick={() => openModal(solicitud.id)}>
-                            Añadir Comentario
-                          </button>
-                      ) : (
+    <div className="container-admin">
+      <h4>Panel de Administrador</h4>
+      {error ? (
+        <p className="error">{error}</p>
+      ) : solicitudes.length === 0 ? (
+        <p>No hay solicitudes pendientes.</p>
+      ) : (
+        <table className="admin-table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Usuario</th>
+              <th>Fecha Inicio</th>
+              <th>Fecha Fin</th>
+              <th>Estado</th>
+              {userRole === "TH" && <th>Aprobada por Líder</th>}
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {solicitudes.map((solicitud) => (
+              <tr key={solicitud.id}>
+                <td>{solicitud.id}</td>
+                <td>{solicitud.usuario.nombre}</td>
+                <td>{solicitud.fechaInicio}</td>
+                <td>{solicitud.fechaFin}</td>
+                <td>{getEstadoSolicitud(solicitud)}</td>
+                {userRole === "TH" && (
+                  <td>
+                    {solicitud.numeroAprobaciones === 1 ||
+                    solicitud.numeroAprobaciones === 2
+                      ? "Sí"
+                      : "No"}
+                  </td>
+                )}
+                <td>
+                  {userRole === "OPERACIONES" ? (
+                    <button onClick={() => openModal(solicitud.id)}>
+                      Añadir Comentario
+                    </button>
+                  ) : (
+                    <>
+                      {!solicitud.rechazado ? (
+                        userRole === "LIDER" ? (
+                          <>
+                            {solicitud.numeroAprobaciones !== 1 && (
+                              <button
+                                onClick={() => handleApprove(solicitud.id)}
+                              >
+                                Aprobar
+                              </button>
+                            )}
+                            <button onClick={() => handleReject(solicitud.id)}>
+                              Rechazar
+                            </button>
+                          </>
+                        ) : userRole === "TH" ? (
+                          solicitud.numeroAprobaciones === 0 ? (
+                            <p>Falta aprobación del Líder</p>
+                          ) : solicitud.estado ? (
+                            <button onClick={() => handleReject(solicitud.id)}>
+                              Rechazar
+                            </button>
+                          ) : (
+                            <>
+                              <button
+                                onClick={() => handleApprove(solicitud.id)}
+                              >
+                                Aprobar
+                              </button>
+                              <button
+                                onClick={() => handleReject(solicitud.id)}
+                              >
+                                Rechazar
+                              </button>
+                            </>
+                          )
+                        ) : (
                           <>
                             <button onClick={() => handleApprove(solicitud.id)}>
                               Aprobar
@@ -198,31 +235,34 @@ const AdminDashboard = () => {
                               Rechazar
                             </button>
                           </>
-                      )}
-                    </td>
-                  </tr>
-              ))}
-              </tbody>
-            </table>
-        )}
+                        )
+                      ) : null}
+                    </>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
 
-        {showModal && (
-            <div className="modal">
-              <div className="modal-content">
-                <h4>Añadir Comentario</h4>
-                <textarea
-                    value={comentario}
-                    onChange={(e) => setComentario(e.target.value)}
-                    placeholder="Escriba un comentario..."
-                ></textarea>
-                <div className="modal-buttons">
-                  <button onClick={handleAddComentario}>Guardar</button>
-                  <button onClick={closeModal}>Cancelar</button>
-                </div>
-              </div>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h4>Añadir Comentario</h4>
+            <textarea
+              value={comentario}
+              onChange={(e) => setComentario(e.target.value)}
+              placeholder="Escriba un comentario..."
+            ></textarea>
+            <div className="modal-buttons">
+              <button onClick={handleAddComentario}>Guardar</button>
+              <button onClick={closeModal}>Cancelar</button>
             </div>
-        )}
-      </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
