ae97e11ede3d7a886b7f7ce5375a4bdc6f8f9f84
CDPATINOCoder <cesardavidpatinovera@gmail.com>
2025-01-08 17:00:13 -0300

    EmailService creado, servicio agregado para mandar correos

--- vacaciones-backend/src/main/java/bootcamp/vacaciones/services/EmailService.java
+++ vacaciones-backend/src/main/java/bootcamp/vacaciones/services/EmailService.java
@@ -13,6 +13,9 @@ public class EmailService {
 
     public void enviarCorreo(String destinatario, String asunto, String contenidoHtml) {
         try {
+            if (!esCorreoValido(destinatario)) {
+                throw new IllegalArgumentException("Correo electrónico no válido: " + destinatario);
+            }
             MimeMessage mensaje = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mensaje, true);
             helper.setTo(destinatario);
@@ -23,4 +26,11 @@ public class EmailService {
             throw new RuntimeException("Error al enviar correo: " + e.getMessage());
         }
     }
+
+    public boolean esCorreoValido(String correo) {
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+        return correo.matches(regex);
+    }
+
+
 }
