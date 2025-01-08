5b8786d6a3935885ce31137f420e9a65fe519338
CDPATINOCoder <cesardavidpatinovera@gmail.com>
2025-01-08 11:18:15 -0300

    EmailService.java creado, servicio para enviar correos con un destinatario, asunto y el mensaje

---
+++ vacaciones-backend/src/main/java/bootcamp/vacaciones/services/EmailService.java
@@ -0,0 +1,26 @@
+package bootcamp.vacaciones.services;
+
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void enviarCorreo(String destinatario, String asunto, String contenidoHtml) {
+        try {
+            MimeMessage mensaje = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mensaje, true);
+            helper.setTo(destinatario);
+            helper.setSubject(asunto);
+            helper.setText(contenidoHtml, true);
+            mailSender.send(mensaje);
+        } catch (Exception e) {
+            throw new RuntimeException("Error al enviar correo: " + e.getMessage());
+        }
+    }
+}
