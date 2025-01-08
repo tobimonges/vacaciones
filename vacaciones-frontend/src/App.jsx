e6f421a41c69d576835e5b31effd984d7827e48c
Sebas <sebaslomba23@gmail.com>
2025-01-08 14:12:33 -0300

    borre ruta duplicada

--- vacaciones-frontend/src/App.jsx
+++ vacaciones-frontend/src/App.jsx
@@ -6,7 +6,6 @@ import NuevaSolicitud from "./components/NuevaSolicitud";
 import SolicitudDetalle from "./components/SolicitudDetalle";
 import PrivateRoute from "./components/PrivateRoute";
 import AdminDashboard from "./components/AdminDashboard";
-import LoginForgotPassword from './components/LoginForgotPassword';
 import RestablecerContraseña from "./components/RestablecerContraseña";
 import LoginForgotPassword from "./components/LoginForgotPassword";
 import CrearUsuario from "./components/CrearUsuario";
