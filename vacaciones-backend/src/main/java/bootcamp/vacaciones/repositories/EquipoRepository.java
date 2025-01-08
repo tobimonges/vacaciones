45422b14ac546ffa0d12806aac3cf747b4b27e7e
LAPTOPACER\evanc <iancuentaszavala>
2025-01-08 15:26:46 -0300

    Creacion de Service, Controller y Repository para Equipos y Cargos

---
+++ vacaciones-backend/src/main/java/bootcamp/vacaciones/repositories/EquipoRepository.java
@@ -0,0 +1,9 @@
+package bootcamp.vacaciones.repositories;
+
+import bootcamp.vacaciones.models.EquipoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipoRepository extends JpaRepository<EquipoModel, Long> {
+}
