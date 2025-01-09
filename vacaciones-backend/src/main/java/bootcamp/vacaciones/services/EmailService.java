e84820fc75b4315c800cd26851ad43a6e22b8c8c
CDPATINOCoder <cesardavidpatinovera@gmail.com>
2025-01-09 14:48:48 -0300

    se agrego la anotacion @Async

--- vacaciones-backend/src/main/java/bootcamp/vacaciones/services/EmailService.java
+++ vacaciones-backend/src/main/java/bootcamp/vacaciones/services/EmailService.java
@@ -4,6 +4,7 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void enviarCorreo(String destinatario, String asunto, String contenidoHtml) {
         try {
             if (!esCorreoValido(destinatario)) {
