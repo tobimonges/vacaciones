879dd41f65e34e3b9a2ca1fc567cfcc413883175
RSID99 <rolodelvalle15@gmail.com>
2025-01-10 15:12:47 -0300

    se modifica UsuarioService, UsuarioRepository para que la cantidad de dias de vacaciones aumente dependiendo de la fecha ingreso

--- vacaciones-backend/src/main/java/bootcamp/vacaciones/repositories/UsuarioRepository.java
+++ vacaciones-backend/src/main/java/bootcamp/vacaciones/repositories/UsuarioRepository.java
@@ -16,9 +16,20 @@ public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
     @Query("SELECT u FROM UsuarioModel u WHERE u.rol.nombre = 'LIDER'")
     List<UsuarioModel> listarLideres();
 
+    @Query(value = "SELECT EXTRACT(YEAR FROM antiguedad) FROM usuarios WHERE id_usuario = :id", nativeQuery = true)
+    int obtenerYears(@Param("id") Long id);
+
+    @Query(value = "SELECT EXTRACT(MONTH FROM antiguedad) FROM usuarios WHERE id_usuario = :id", nativeQuery = true)
+    int obtenerMonths(@Param("id") Long id);
+
+    @Query(value = "SELECT EXTRACT(DAY FROM antiguedad) FROM usuarios WHERE id_usuario = :id", nativeQuery = true)
+    int obtenerDays(@Param("id") Long id);
+
     @Modifying
     @Transactional
     @Query(value = "UPDATE usuarios u SET antiguedad = AGE(CURRENT_DATE, u.fecha_ingreso) WHERE u.id_usuario = :id", nativeQuery = true)
     void actualizarAntiguedad(@Param("id") Long id);
     List<UsuarioModel> findByRolNombre(String rolNombre);
+
+
 }
