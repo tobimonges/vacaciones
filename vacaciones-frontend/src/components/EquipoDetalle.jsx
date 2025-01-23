206c70540e4dc30fb57805333a699cd9c72cfb27
FaffoPistilli <fabricciopistilli40@gmail.com>
2025-01-23 17:30:00 -0300

    Ajustes botones editar y filtro de busqueda EquipoDetalle

--- vacaciones-frontend/src/components/EquipoDetalle.jsx
+++ vacaciones-frontend/src/components/EquipoDetalle.jsx
@@ -202,6 +202,7 @@ const EquipoDetalle = () => {
                   placeholder="Nombre del equipo"
                   value={filterText}
                   onChange={handleFilterChange}
+                  class="inputCreate"
                 />
               </div>
               <div className="content-section-detalle">
@@ -247,6 +248,7 @@ const EquipoDetalle = () => {
                                 </button>
                                 <button
                                   className="cancel-button"
+                                  style={{ backgroundColor: "#ff6e33" }}
                                   onClick={() => {
                                     setEditingId(null); // Salir del modo edición
                                     setNewName(""); // Opcional: Limpiar el estado del nuevo nombre
