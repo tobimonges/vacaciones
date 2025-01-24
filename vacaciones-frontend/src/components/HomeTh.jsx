97c7ccb0339305085ef9c781d629fa7a832a10ff
tobimonges <tobiasmonges@pm.me>
2025-01-24 14:07:15 -0300

    cambios en el espacio de solicitud

--- vacaciones-frontend/src/components/HomeTh.jsx
+++ vacaciones-frontend/src/components/HomeTh.jsx
@@ -372,7 +372,7 @@ const HomeTh = () => {
                     ) : (
                       <span>
                         {`Tienes `}
-                        <span className="pending-count-number">{`${pendingCount}`}</span>
+                        <span className="pending-count-number">{` ${pendingCount} `}</span>
                         {pendingCount === 1
                           ? ` solicitud pendiente!`
                           : ` solicitudes pendientes!`}
